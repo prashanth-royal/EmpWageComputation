43a536696706806bb760a474cc86586bb2c08338
Prashanth <prashanth.royal2046@gmail.com>
2021-03-25 13:34:04 +0530

    Added: UC6-CalculateWageTillConditionReached

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,25 +1,39 @@
 #!/bin/bash -x
 
+# constants
 IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HR=20
-numWorkingDays=10
-totalWorkingHrs=0
+MAX_HRS_IN_MONTH=50
+NUM_WORKING_DAYS=20
 
-for ((day=1; day<=$numWorkingDays; day++))
-do
-   empCheck=$((RANDOM%3))
-   case $empCheck in
+# variables
+totalEmpHrs=0
+totalWorkingDays=0
+
+
+function getWorkingHrs() {
+ empCheck=$((RANDOM%3))
+   case $1 in
+
       $IS_FULL_TIME)
-      totalWorkingHrs=$(($totalWorkingHrs+8))
+         workHrs=8
       ;;
       $IS_PART_TIME)
-      totalWorkingHrs=$(($totalWorkingHrs+4))
+         workHrs=4
       ;;
       *)
-      totalWorkingHrs=$(($totalWorkingHrs+0))
+         workHrs=0
       ;;
    esac
+   echo $workHrs
+}
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+
+   ((totalWorkingDays++))
+   workHrs="$( getWorkingHrs $((RANDOM%3)) )"
+   totalEmpHrs=$(($totalEmpHrs+$workHrs))
 done
-salary=$(($EMP_RATE_PER_HR*$totalWorkingHrs))
-echo $salary
+   totalSalary=$(($EMP_RATE_PER_HR*$totalEmpHrs))
+   echo $totalSalary
